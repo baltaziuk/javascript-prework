844b8a8e57a819fb248d58c11c7daf9c4c5cf73e
Dariusz Baltaziuk <dariusz@baltaziuk.eu>
2020-06-30 20:15:39 +0200

    code improvement

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
+
 if(randomNumber == 1){
     computerMove = 'kamień';
 }
-if(randomNumber == 2){
+else if(randomNumber == 2){
     computerMove = 'papier';
 }
-if(randomNumber == 3){
+else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 
